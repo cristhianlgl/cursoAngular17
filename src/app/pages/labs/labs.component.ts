ea864419bcd4ed8385180a2b653c4b99a16c9093
cristhianlgl <cristhianlgl@hotmail.com>
2024-05-08 21:59:24 -0500

    cap 3 - 6

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -9,5 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LabsComponent {
   title = 'todoapp';
-  lista = ['task1','task2','task3','task4']
+  lista = ['task1','task2','task3','task4'];
+  botonActivo = false;
+  persona = {
+    nombre: 'cristian',
+    edad:'100',
+    img:'https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg'
+  }
+
+  mostarAlerta(texto:string){
+    alert(texto)
+  }
+
+  cambioNumero(event:Event){
+    const input = event.target as HTMLInputElement
+    this.persona.edad = input.value
+  }
+
 }
