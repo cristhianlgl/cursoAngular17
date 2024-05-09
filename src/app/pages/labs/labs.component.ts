58132b21a67b8576970fd70a8773ace3050df89a
cristhianlgl <cristhianlgl@hotmail.com>
2024-05-08 21:11:11 -0500

    cap 1 y 2

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  title = 'todoapp';
+  lista = ['task1','task2','task3','task4']
+}
